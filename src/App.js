9e16f62ffc6c8e00052204b144fe85818c13b513
PhatFunny <ntphat.funny@gmail.com>
2022-10-11 13:16:59 +0700

    Lesson 60

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
-import Button from './component/button';
-
 function App() {
   return (
     <div className="App">
       <h1>Hello ae</h1>
-      <Button />
     </div>
   );
 }
